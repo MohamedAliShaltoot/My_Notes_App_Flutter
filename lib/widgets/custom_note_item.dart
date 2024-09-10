b866086966519aa129d765ffde48458ece73053d
MohamedAliShaltoot <mohamedali3092002@gmail.com>
2024-09-11 01:48:09 +0300

    add custom note item and notes view body

---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 16,top: 26,bottom: 24),
+      decoration: BoxDecoration(
+        color: Colors.cyan,
+        borderRadius: BorderRadius.circular(16)
+      ),
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+ListTile(
+  title: const Text("FLutter language",style: TextStyle(color: Colors.black,fontSize: 28),),
+  subtitle:  Padding(
+    padding: const EdgeInsets.only(top: 16,bottom: 24),
+    child: Text("Hi i am Mohamed Ali",style: TextStyle(color: Colors.black.withOpacity(0.5),fontSize: 20),),
+  ),
+  trailing: IconButton(onPressed: (){}, icon: const Icon(Icons.delete,color: Colors.black,size: 33,)),
+  
+),
+ Padding(
+   padding: const EdgeInsets.only(right: 24),
+   child: Text("May21,2024",style: TextStyle(color: Colors.black.withOpacity(0.5)),),
+ ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -15,6 +16,8 @@ class NotesViewBody extends StatelessWidget {
           SizedBox(height: 50,),
       
       CustomAppBar(),
+      NoteItem(),
+
         ],
       ),
     );
