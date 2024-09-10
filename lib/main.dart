8111c599eb0d4f9d2e06c5ea1bc5ea16b390d6a8
MohamedAliShaltoot <mohamedali3092002@gmail.com>
2024-09-11 00:50:32 +0300

    disenables debuhshowCheckModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyNotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       
       home:Scaffold(),
     );
