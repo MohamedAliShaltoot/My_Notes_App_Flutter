83c3981c7beb656bb0176e55d45e9284220ad0c4
MohamedAliShaltoot <mohamedali3092002@gmail.com>
2024-09-11 00:24:52 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyNotesApp());
+}
+
+class MyNotesApp extends StatelessWidget {
+  const MyNotesApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      
+      home:Scaffold(),
+    );
+  }
+}
+
